e0300e89c7c0a059d552cec3ac1b2dabe6a8347e
Luiz Branco <me@luizbranco.com>
2013-07-09 20:50:59 -0300

    Add grunt for continous testing

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    regarde: {
+      app: {
+        files: ['lib/**/*.js', 'routes/**/*.js', 'test/**/*.js'],
+        tasks: ['exec']
+      }
+    },
+    exec: {
+      test: {
+        cmd: 'mocha --recursive --colors --growl --reporter min'
+      }
+    }
+  });
+
+  // Default task.
+  grunt.registerTask('default', 'regarde');
+
+  // Load Dependencies
+  grunt.loadNpmTasks('grunt-regarde');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-exec');
+
+};
